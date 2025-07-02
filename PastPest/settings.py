fdee54154fc0affe44a8894d5191b65619a73909
sw245 <qtd2012@gmail.com>
2025-07-02 14:45:46 +0900

    템플릿 업데이트(부분)
    마이페이지, 상세페이지, 쇼츠 업로드 페이지 추가

--- PastPest/settings.py
+++ PastPest/settings.py
@@ -1,5 +1,5 @@
 
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -128,8 +128,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR,'static'),
+)
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / "media"
