a90195b5555cac9db1190b05c6f7d7ad9e4eac47
321qas <321qas@naver.com>
2025-07-03 16:49:36 +0900

    0703
    db 변경

--- festivals/models.py
+++ festivals/models.py
@@ -31,6 +31,7 @@ class Festival(models.Model):
         max_digits=10, decimal_places=7, blank=True, null=True,
         help_text="축제 위치의 경도를 입력하세요. (예: 126.9779692)")
     fee = models.CharField(
+        max_length=70,
         default=0,
         help_text="참가 요금을 원 단위로 입력하세요. (무료이면 0 입력)")
     organizer = models.CharField(
