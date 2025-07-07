0b007beeea0fb4162c76177daa04c84de3a6c15f
321qas <321qas@naver.com>
2025-07-07 16:28:49 +0900

    0707

--- tags/models.py
+++ tags/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 class Tag(models.Model):                                    # 관심 태그
     name = models.CharField(max_length=30, unique=True)
-    # 문화예술, 전통역사, 자연생태, 지역특산물, 먹거리/푸드, 음악/공연, 체험/참여, 봄, 여름, 가을, 겨울, 가족, 어린이, 청소년, 가족, 성인, 어르신, 전연령 / 총 18개
+    # 문화예술, 전통역사, 자연생태, 지역특산물, 먹거리/푸드, 음악/공연, 체험/참여, 봄, 여름, 가을, 겨울, 가족, 어린이, 청소년, 성인, 어르신, 전연령 / 총 17개
 
     def __str__(self):
         return self.name
