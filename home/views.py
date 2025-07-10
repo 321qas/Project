eb2b3f8da01d80f1a03398e6bae3f838f8b83739
Leaf_yj <yukarina11@korea.ac.kr>
2025-07-10 16:21:55 +0900

    0710

--- home/views.py
+++ home/views.py
@@ -5,7 +5,12 @@ from django.db.models import Prefetch
 from tags.models import Tag
 
 def index(request):
-    return render(request,'index.html')
+    user_id = request.session.get('user_id')
+    nickname = request.session.get('nickname')
+    return render(request, 'index.html', {
+        'user_id': user_id,
+        'nickname': nickname,
+    })
 
 def jlist(request):
     qs = Festival.objects.filter(
