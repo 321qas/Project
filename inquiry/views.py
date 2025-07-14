a42d29856b4beaf87bdd08c132cdc0d205fa5295
GimGadasian <sfa2code@gmail.com>
2025-07-14 18:28:03 +0900

    write and store in db

--- inquiry/views.py
+++ inquiry/views.py
@@ -16,12 +16,58 @@ def user_support(request):
 
 def my_posts(request):
     user_id = request.session.get('user_id')
-    inquiries = Inquiry.objects.filter(user__user_id=user_id).values(
-        'user__nickname', 'title', 'created_at', 'status', 'content'
-    )
-    data = list(inquiries)
+    inquiries = Inquiry.objects.filter(user__user_id=user_id).prefetch_related('replies', 'user')
+
+    data = []
+    for inquiry in inquiries:
+        reply = inquiry.replies.first()  
+        data.append({
+            'id': inquiry.id,
+            'title': inquiry.title,
+            'content': inquiry.content,
+            'status': inquiry.status,
+            'created_at': inquiry.created_at.isoformat(),
+            'user__nickname': inquiry.user.nickname if inquiry.user else "탈퇴한 회원입니다",
+            # 없을 때
+            'answer': reply.content if reply else "아직 답변이 등록되지 않았습니다.",
+            # 디테일하게 수정할 때
+            'admin_nickname': reply.admin.nickname if reply and reply.admin else "관리자 미표시",
+            'answer_created_at': format(reply.created_at, 'Y-m-d H:i') if reply else "작성일 없음"
+        })
+
+    return JsonResponse({'inquiries': data})
+
+def all_posts(request):
+    inquiries = Inquiry.objects.select_related('user').prefetch_related('replies').order_by('-created_at')
+
+    data = []
+    for inquiry in inquiries:
+        reply = inquiry.replies.first()
+        data.append({
+            'title': inquiry.title,
+            'content': inquiry.content,
+            'status': inquiry.status,
+            'created_at': inquiry.created_at.isoformat(),
+            'user__nickname': inquiry.user.nickname if inquiry.user else "탈퇴한 회원입니다",
+            'answer': reply.content if reply else "아직 답변이 등록되지 않았습니다.",
+        })
+
     return JsonResponse({'inquiries': data})
     
 
 def inquiry_write(request):
-    return render(request, 'inquiry_write.html')
+    
+    if request.method == 'POST':
+        title = request.POST.get('title')
+        content = request.POST.get('content')
+        Inquiry.objects.create(
+            user=request.user,
+            title=title,
+            content=content,
+            status='waiting',
+        )
+        return redirect('/inquiry/support/')  # 문의글 목록 페이지 URL 이름
+    else:
+        return render(request, 'inquiry_write.html')
+        
+    
